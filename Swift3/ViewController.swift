f22d1d2e01946a6b5e1e9a8096cccbc10bcab5b8
vijayalakshmi <vijayalakshmi.pulivarthi@sourcebits.com>
2016-09-19 18:58:35 +0530

    Initial Commit

---
+++ Swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift3
+//
+//  Created by Vijayalakshmi Pulivarthi on 19/09/16.
+//  Copyright © 2016 sourcebits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
